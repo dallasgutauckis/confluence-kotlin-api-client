cb96dd4198225fcf603e06f82cded4b53280b55b
Dallas Gutauckis <dallas@gutauckis.com>
2018-10-01 00:36:22 -0400

    Adding some basic token auth

--- src/main/kotlin/com/dallasgutauckis/confluence/ConfluenceClient.kt
+++ src/main/kotlin/com/dallasgutauckis/confluence/ConfluenceClient.kt
@@ -9,10 +9,12 @@ import retrofit2.adapter.java8.Java8CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-class ConfluenceClient(private val baseUrl: String,
-                       private val auth: Authentication?,
-                       private val logger: ((message: String) -> Unit)? = null,
-                       private val logLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.NONE) {
+class ConfluenceClient(
+    baseUrl: String,
+    auth: Authentication?,
+    logger: ((message: String) -> Unit)? = null,
+    logLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.NONE
+) {
     private val restClient: ConfluenceService
 
     init {
@@ -20,36 +22,64 @@ class ConfluenceClient(private val baseUrl: String,
 
         if (auth != null) {
             okHttpClient.addInterceptor { chain ->
-                chain.proceed(chain.request().newBuilder()
-                        .addHeader("Authorization", Credentials.basic(auth.user, auth.token))
-                        .build())
+                chain.proceed(
+                    chain.request().newBuilder()
+                        .addHeader("Authorization", auth.getCredentials())
+                        .build()
+                )
             }
         }
 
         // Add last so it logs everything
         if (logger != null) {
-            val loggingInterceptor = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { message -> logger.invoke(message) })
+            val loggingInterceptor =
+                HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { message -> logger.invoke(message) })
             loggingInterceptor.level = logLevel
             okHttpClient.addNetworkInterceptor(loggingInterceptor)
         }
 
         restClient = Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .client(okHttpClient.build())
-                .addConverterFactory(GsonConverterFactory.create(Gson()))
-                .addCallAdapterFactory(Java8CallAdapterFactory.create())
-                .build()
-                .create(ConfluenceService::class.java)
+            .baseUrl(baseUrl)
+            .client(okHttpClient.build())
+            .addConverterFactory(GsonConverterFactory.create(Gson()))
+            .addCallAdapterFactory(Java8CallAdapterFactory.create())
+            .build()
+            .create(ConfluenceService::class.java)
     }
 
     fun methods(): ConfluenceMethods {
         return ConfluenceMethodsRetrofit(restClient)
     }
 
-    data class Authentication(val user: String,
-                              val token: String) {
+    interface Authentication {
+        fun getCredentials(): String
+    }
+
+    data class BasicAuthentication(
+        val user: String,
+        val token: String
+    ) : Authentication {
+        override fun getCredentials(): String = Credentials.basic(user, token)
+
         override fun toString(): String {
-            return "%s:%s[...]%s".format(user, token.subSequence(0, 3), token.subSequence(token.length - 4, token.length - 1))
+            return "Basic %s:%s[...]%s".format(
+                user,
+                token.subSequence(0, 3),
+                token.subSequence(token.length - 4, token.length - 1)
+            )
+        }
+    }
+
+    data class TokenAuthentication(
+        val token: String
+    ) : Authentication {
+        override fun getCredentials(): String = "Bearer $token"
+
+        override fun toString(): String {
+            return "Bearer %s[...]%s".format(
+                token.subSequence(0, 3),
+                token.substring(token.length - 4, token.length - 1)
+            )
         }
     }
 }
